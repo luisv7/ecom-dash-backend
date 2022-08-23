811c358a12052c775ef15d3cc95a47b1612500ae
luisv7 <luisvillarreal_7@icloud.com>
2022-08-23 02:11:14 -0700

    wip: removed stockMessage

--- models/product.js
+++ models/product.js
@@ -6,10 +6,9 @@ const products = new Schema({
     cost: Number,
     price: Number,
     stock: Number,
-    stockMessage: String,
     sku: String,
     description: String,
-    categories: String,
+    category: String,
     image: {type: String, default:'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081'},
 },{timestamps: true});
 
