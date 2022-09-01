3a4347bb40a0a1fc00a8e47fd187956838e5c724
luisv7 <luisvillarreal_7@icloud.com>
2022-08-31 23:30:47 -0700

    wip: added create, update, and delete routes

--- controllers/products.js
+++ controllers/products.js
@@ -22,4 +22,27 @@ router.post('/', async (req, res) => {
     }
 })
 
+// Update
+router.put('/:id', async (req, res) => {
+    try {
+        res.status(200).json(await Products.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true } // send new updated version of the object
+        ))
+    } catch (error) {
+        res.status(400).json({message: 'bad request' })
+    }
+})
+
+// Delete
+router.delete('/:id', async (req,res) => {
+    console.log(req.params.id)
+    try {
+        res.status(200).json(await Products.findByIdAndDelete(req.params.id))
+    } catch (error) {
+        res.status(400).json({message: 'bad request' })
+    }
+})
+
 module.exports = router;
